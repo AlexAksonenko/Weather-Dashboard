0773ccc66f959a703e7b7c6e5daf24ad214408f3
Alex Aksonenko <aksenenkos@gmail.com>
2022-05-01 18:33:57 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,5 @@
-// ** START PSEUDO CODE (subject to change) ** //
 
-// when user searches for a city (clicks search button):
-//  - store the user input in a variable
-//  - use a fetch api to get the current & future conditions for that city
-//  - store that city into local storage
-// use the data from fetch to populate in the current-weather container:
-//  - name and today's date as M/DD/YYY
-//  - temp
-//  - wind
-//  - humidity
-//  - UV index (color coded for favorable(green), moderate(yellow), or severe(red))
-// use the data from fetch to populate in the five-day container:
-//  - date
-//  - an icon reprsentation of weather conditions
-//  - the temp
-//  - wind speed
-//  - humidity
-// use data in local.storage to create a button under the <hr> in search area for city history
-//  - when you click the button it displays the current and future conditions for that city
-
-// ** END PSEUDO CODE ** //
-
-// START GLOBAL VARIABLES //
+// GLOBAL VARIABLES //
 
 var openWeatherApiKey = '26ba3a7e283acb9cd1e8665c6c3b319a';
 var openWeatherCoordinatesUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
@@ -35,7 +13,6 @@ var currentDay = moment().format('M/DD/YYYY');
 const weatherIconUrl = 'http://openweathermap.org/img/wn/';
 var searchHistoryArray = loadSearchHistory();
 
-// END GLOBAL VARIABLES //
 
 // Define function to capitalize the first letter of a string
 function titleCase(str) {
